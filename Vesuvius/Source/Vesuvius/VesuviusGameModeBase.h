4671c03914dc2477ab053d193365ab381e3891d3
CGrogan4 <Cassidiegrogan@gmail.com>
2025-10-20 18:33:55 -0400

    Add World Map

---
+++ Vesuvius/Source/Vesuvius/VesuviusGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VesuviusGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VESUVIUS_API AVesuviusGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
